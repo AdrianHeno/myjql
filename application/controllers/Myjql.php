c09f4dcd1ab401c092275207f8e2e14a9fd7b596
SaintGrey <adrian.heno@gmail.com>
2017-09-14 15:32:27 +1000

    Test slack

--- application/controllers/Myjql.php
+++ application/controllers/Myjql.php
@@ -169,6 +169,38 @@ class Myjql extends CI_Controller {
 	}
 	
 	function burndown($username, $password, $project){//This is a proof of concept and will need to be reworked if deemed worth the effort
+		
+		$slack_json = '{
+					"attachments": [
+						{
+							"fallback": "Required plain-text summary of the attachment.",
+							"color": "#36a64f",
+							"pretext": "Optional text that appears above the attachment block",
+							"author_name": "Bobby Tables",
+							"author_link": "http://flickr.com/bobby/",
+							"author_icon": "http://flickr.com/icons/bobby.jpg",
+							"title": "Slack API Documentation",
+							"title_link": "https://api.slack.com/",
+							"text": "Optional text that appears within the attachment",
+							"fields": [
+								{
+									"title": "Priority",
+									"value": "High",
+									"short": false
+								}
+							],
+							"image_url": "http://my-website.com/path/to/image.jpg",
+							"thumb_url": "http://example.com/path/to/thumb.png",
+							"footer": "Slack API",
+							"footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+							"ts": 123456789
+						}
+					]
+				}';
+
+	echo $slack_json;
+		die();
+		
 		$current_sprint_id = $this->get_current_sprint_id($username, $password, $project);
 		
 		set_time_limit(600);
@@ -290,36 +322,6 @@ class Myjql extends CI_Controller {
 		//Send encode and send the payload
 		echo json_encode($slack_payload);
 		*/
-		
-		$slack_json = '{
-    "attachments": [
-        {
-            "fallback": "Required plain-text summary of the attachment.",
-            "color": "#36a64f",
-            "pretext": "Optional text that appears above the attachment block",
-            "author_name": "Bobby Tables",
-            "author_link": "http://flickr.com/bobby/",
-            "author_icon": "http://flickr.com/icons/bobby.jpg",
-            "title": "Slack API Documentation",
-            "title_link": "https://api.slack.com/",
-            "text": "Optional text that appears within the attachment",
-            "fields": [
-                {
-                    "title": "Priority",
-                    "value": "High",
-                    "short": false
-                }
-            ],
-            "image_url": "http://my-website.com/path/to/image.jpg",
-            "thumb_url": "http://example.com/path/to/thumb.png",
-            "footer": "Slack API",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            "ts": 123456789
-        }
-    ]
-}';
-
-	echo $slack_json;
 	}
 	
 	
