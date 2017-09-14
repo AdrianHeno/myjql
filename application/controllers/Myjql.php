96b7052102e4c90754a0c54de3f4782daf003f65
SaintGrey <adrian.heno@gmail.com>
2017-09-14 16:29:20 +1000

    Fixed up some formatting

--- application/controllers/Myjql.php
+++ application/controllers/Myjql.php
@@ -293,9 +293,9 @@ class Myjql extends CI_Controller {
 			array (
 				0 =>
 				array(
-					'fallback' => $_GET['text'] . 'Burndown Chart',
+					'fallback' => $_GET['text'] . ' Current Sprint Burndown Chart',
 					'color' => '#36a64f',
-					'title' => $_GET['text'] . 'Burn Down',
+					'title' => $_GET['text'] . ' Current Sprint Burn Down',
 					'title_link' => $chart_url,
 					'image_url' => $chart_url,
 					'thumb_url' => $chart_url
